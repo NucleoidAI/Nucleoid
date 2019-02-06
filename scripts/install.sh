f79c2689ce78e0f1bd5fc9f78d70d5ff5ea44e52
Can Mingir <canmingir@gmail.com>
2019-02-06 20:45:36 +0000

    Set name and email in Git during installation

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,3 +2,8 @@
 sudo apt update
 sudo apt install nodejs -y
 sudo apt install npm -y
+
+read -p "user.name: " name
+git config --global user.name "$name"
+read -p "user.email: " email
+git config --global user.email "$email"
