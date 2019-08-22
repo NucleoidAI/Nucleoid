266d218e16d10ba26c5ad7d3b9aec5e12aea947f
Can Mingir <canmingir@gmail.com>
2019-08-22 16:28:36 +0000

    Initialize performance tests

--- test/nucleoid.spec.js
+++ test/nucleoid.spec.js
@@ -2,6 +2,8 @@ var assert = require("assert");
 var nucleoid = require("../nucleoid");
 
 describe("Nucleoid", function() {
+  this.slow(1);
+
   it("runs statements in the state", function() {
     nucleoid.run("var i = 1 ;");
     assert.equal(nucleoid.run("i == 1"), true);
