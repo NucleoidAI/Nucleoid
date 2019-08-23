5dcbb5ae766df0aeb2e1ef6245b04c0d0ee389c3
Can Mingir <canmingir@gmail.com>
2019-08-23 15:27:22 +0000

    Persist statement

--- nucleoid.js
+++ nucleoid.js
@@ -1,7 +1,9 @@
 var stack = require("./stack");
 var Statement = require("./statement");
+const fs = require("fs");
 
 module.exports.run = function(string) {
   let statements = Statement.compile(string);
+  fs.appendFileSync("data", string + "\n");
   return stack.process(statements);
 };
