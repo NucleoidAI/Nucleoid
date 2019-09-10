b5c5fa058c0c5ec266a59f0fc0c447ac46fc1ca4
Can Mingir <canmingir@gmail.com>
2019-09-10 08:36:10 +0000

    Run code format and linting validations before commit

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,3 +8,6 @@ read -p "user.name: " name
 git config --global user.name "$name"
 read -p "user.email: " email
 git config --global user.email "$email"
+
+sudo npm install prettier -g
+sudo npm install eslint -g
