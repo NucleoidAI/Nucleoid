71cb9ad695f79221c1f35c0c25b4aa700b5e34e5
Can Mingir <canmingir@gmail.com>
2019-11-23 15:03:27 +0000

    Fix creating standard built-in object with var keyword

--- es6/es6$variable.js
+++ es6/es6$variable.js
@@ -16,18 +16,24 @@ module.exports = function ES6$VARIABLE(string, offset) {
 
   context = Token.next(string, context.offset);
   let name = context.token;
-  context = Token.next(string, context.offset);
-
+  let standard = (context = Token.next(string, context.offset));
   let check = Token.next(string, context.offset);
-  if (check.token === "new") {
+
+  standard: if (check.token === "new") {
     context = Token.next(string, check.offset);
+
+    if (eval("typeof " + context.token) === "function") {
+      context = standard;
+      break standard;
+    }
+
     let instance = $INSTANCE(context.token);
     context = Token.next(string, context.offset);
     context = Token.next(string, context.offset);
     return { statement: $VAR(name, instance), offset: context.offset };
-  } else {
-    context = $VALUE(string, context.offset);
-    let expression = context.statement;
-    return { statement: $VAR(name, expression), offset: context.offset };
   }
+
+  context = $VALUE(string, context.offset);
+  let expression = context.statement;
+  return { statement: $VAR(name, expression), offset: context.offset };
 };
