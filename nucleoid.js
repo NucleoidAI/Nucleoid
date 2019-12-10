9340fea5890427c16c7139155d3c78675bc8f9e3
Can Mingir <canmingir@gmail.com>
2019-12-10 13:37:17 +0000

    Return details of run if set

--- nucleoid.js
+++ nucleoid.js
@@ -2,8 +2,16 @@ var stack = require("./stack");
 var Statement = require("./statement");
 const fs = require("fs");
 
-module.exports.run = function(string) {
+module.exports.run = function(string, details) {
+  let before = Date.now();
   let statements = Statement.compile(string);
+  let result = stack.process(statements);
   fs.appendFileSync("data", string + "\n");
-  return stack.process(statements);
+  let after = Date.now();
+
+  if (details === true) {
+    return { result, statements, time: after - before };
+  } else {
+    return result;
+  }
 };
