00b428dc24a6bc2a9e019c124b61228c8f440e99
Can Mingir <canmingir@gmail.com>
2020-01-08 10:48:28 +0000

    Add option to run cache only

--- nucleoid.js
+++ nucleoid.js
@@ -2,12 +2,15 @@ var stack = require("./stack");
 var Statement = require("./statement");
 const fs = require("fs");
 
-module.exports.run = function(string, details) {
+module.exports.run = function(string, details, cacheOnly) {
   let before = Date.now();
 
   let statements = Statement.compile(string);
   let result = stack.process(statements);
-  fs.appendFileSync("data", string + "\n");
+
+  if (!cacheOnly) {
+    fs.appendFileSync("data", string + "\n");
+  }
 
   let after = Date.now();
 
