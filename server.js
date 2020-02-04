120006f99f67a4de86b329214dd91660296f54ac
Can Mingir <canmingir@gmail.com>
2020-02-04 08:25:35 +0000

    Apply statement to all processes

--- server.js
+++ server.js
@@ -14,37 +14,54 @@ app.use(bodyParser.text({ type: "*/*" }));
 const fork = require("child_process").fork;
 var processes = [];
 
-app.post("/", req => {
+app.post("/", (req, res) => {
   let header = req.get("Authorization");
-  let parts = header.split(" ");
-  var payload = jwt.decode(parts[1]);
 
-  let key = payload.username;
-  let proc = processes[key];
+  if (header !== undefined) {
+    let parts = header.split(" ");
+    var payload = jwt.decode(parts[1]);
 
-  if (proc === undefined) {
-    let pid = fork("./process.js", [`--path=${dir}${key}`]);
+    let key = payload.username;
+    let proc = processes[key];
 
-    proc = { pid, requests: [req] };
-    processes[key] = proc;
+    if (proc === undefined) {
+      let pid = fork("./process.js", [`--path=${dir}${key}`]);
 
-    proc.pid.on("message", function(message) {
-      let request = proc.requests.shift();
-      request.res.send(message);
+      proc = { pid, requests: [req] };
+      processes[key] = proc;
 
-      if (proc.requests.length > 0) {
-        let r = proc.requests[0];
-        proc.pid.send(r.body);
-      }
-    });
+      proc.pid.on("message", function(message) {
+        let request = proc.requests.shift();
 
-    proc.pid.send(req.body);
-  } else {
-    proc.requests.push(req);
+        if (request.get("Authorization") !== undefined) {
+          request.res.send(message);
+        }
 
-    if (proc.requests.length === 1) {
+        if (proc.requests.length > 0) {
+          let r = proc.requests[0];
+          proc.pid.send(r.body);
+        }
+      });
+
       proc.pid.send(req.body);
+    } else {
+      proc.requests.push(req);
+
+      if (proc.requests.length === 1) {
+        proc.pid.send(req.body);
+      }
     }
+  } else {
+    for (let p in processes) {
+      let proc = processes[p];
+      proc.requests.push(req);
+
+      if (proc.requests.length === 1) {
+        proc.pid.send(req.body);
+      }
+    }
+
+    res.status(202).end();
   }
 });
 
