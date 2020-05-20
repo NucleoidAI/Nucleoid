b0614a533ff9d12685c6392355bf8ec8db364f4b
Can Mingir <canmingir@gmail.com>
2020-05-20 07:45:14 +0000

    Configure packaging for Launchpad

---
+++ scripts/package.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+mkdir -p nucleoid/source/usr/lib/
+git clone https://gitlab.com/nucleoid/nucleoid.git nucleoid/source/usr/lib/nucleoid/
+npm install --prefix nucleoid/source/usr/lib/nucleoid/
+rm -Rf nucleoid/source/usr/lib/nucleoid/.git
+
+mkdir -p nucleoid/debian
+mv nucleoid/source/usr/lib/nucleoid/scripts/debian/* nucleoid/debian/
+
+mkdir -p nucleoid/source/var/lib/nucleoid/init/
+
+mkdir -p nucleoid/source/etc/nucleoid/
+echo "{}" > nucleoid/source/etc/nucleoid/configuration.json
