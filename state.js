8f2ecafdaabbe010a1c34924286d447277b4df2e
Can Mingir <canmingir@gmail.com>
2020-07-20 08:08:32 +0000

    Retrieve graph information of statement

--- state.js
+++ state.js
@@ -1,10 +1,40 @@
 var state = {}; // eslint-disable-line no-unused-vars
-var transaction = require("./transaction");
+var _transaction = require("./transaction");
 var message = require("./message").message; // eslint-disable-line no-unused-vars
+var _graph = require("./graph");
+
+// eslint-disable-next-line no-unused-vars
+function graph(id) {
+  let node = _graph[id];
+
+  if (_graph[id] === undefined) {
+    throw ReferenceError(`${id} is not defined`);
+  }
+
+  let object = {};
+
+  for (let prop in node) {
+    let n = node[prop];
+
+    if (typeof n === "object") {
+      object[prop] = {};
+
+      for (let nprop in n) {
+        if (typeof n[nprop] !== "object") {
+          object[prop][nprop] = n[nprop];
+        }
+      }
+    } else {
+      object[prop] = node[prop];
+    }
+  }
+
+  return object;
+}
 
 module.exports.state = state;
 module.exports.assign = function(scope, variable, expression) {
-  transaction.register(variable, expression, scope);
+  _transaction.register(variable, expression, scope);
 };
 
 module.exports.run = function(scope, expression) {
