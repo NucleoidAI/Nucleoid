f3aa49f23697b9103ae789b267163397edcb2e6f
Can Mingir <canmingir@gmail.com>
2021-12-15 18:51:29 -0500

    Initialize Express wrapper

--- index.js
+++ index.js
@@ -5,18 +5,28 @@ const cors = require("cors");
 
 const preset = [];
 
+const terminal = express();
+terminal.use(express.text({ type: "*/*" }));
+terminal.use(cors());
+
+terminal.post("/", (req, res) => res.send(runtime.process(req.body)));
+terminal.use((err, res) => {
+  res.type("txt");
+  res.status(500).send(err.stack);
+});
+
 const start = (options) => {
   options = options || {};
 
   const process = require("./src/process");
-  const { terminal } = process.options(options);
+  options = process.options(options);
 
   setImmediate(() => {
     preset.forEach(({ fn, options }) => run(fn.toString(), options));
   });
 
-  if (terminal === undefined || terminal === true) {
-    app.listen(8448);
+  if (options.terminal === undefined || options.terminal === true) {
+    terminal.listen(8448);
   }
 };
 
@@ -30,9 +40,9 @@ const run = (statement, p2) => {
     return runtime.process(`${statement}`, options);
   } else {
     let scope = p2;
-    scope = scope ? `let scope=${JSON.stringify(scope)};` : "";
-
-    const { fn } = parseFn(statement.toString());
+    const { args, fn } = parseFn(statement.toString());
+    scope =
+      scope && args.length ? `let ${args[0]}=${JSON.stringify(scope)};` : "";
     return runtime.process(`${scope}${fn}`);
   }
 };
@@ -67,13 +77,38 @@ const parseFn = (string) => {
 };
 
 const app = express();
-app.use(express.text({ type: "*/*" }));
+app.use(express.json());
 app.use(cors());
 
-app.post("/", (req, res) => res.send(runtime.process(req.body)));
-app.use((err, res) => {
-  res.type("txt");
-  res.status(500).send(err.stack);
+const accept = (req, res, fn) => {
+  const scope = { params: req.params, query: req.query, body: req.body };
+  const result = run(fn, scope);
+  res.send(result);
+};
+
+module.exports = () => ({
+  use: (fn) => app.use(fn),
+  get: (string, fn) => app.get(string, (req, res) => accept(req, res, fn)),
+  post: (string, fn) => app.post(string, (req, res) => accept(req, res, fn)),
+  put: (string, fn) => app.put(string, (req, res) => accept(req, res, fn)),
+  delete: (string, fn) =>
+    app.delete(string, (req, res) => accept(req, res, fn)),
+  listen: (port, fn) => {
+    app.all("*", (req, res) => {
+      res.status(404).end();
+    });
+
+    app.use((err, req, res, next) => {
+      if (!err) next();
+      else if (!(err instanceof Error)) res.status(400).send({ error: err });
+      else res.status(500).send({ error: err.message });
+    });
+
+    start();
+    app.listen(port, fn);
+  },
 });
 
-module.exports = { start, register, run };
+module.exports.start = start;
+module.exports.register = register;
+module.exports.run = run;
--- server.js
+++ server.js
@@ -2,16 +2,16 @@ const express = require("express");
 const cors = require("cors");
 const service = require("./service");
 
-const app = express();
-app.use(express.text({ type: "*/*" }));
-app.use(cors());
+const terminal = express();
+terminal.use(express.text({ type: "*/*" }));
+terminal.use(cors());
 
 service.start("main");
 
-app.post("/", (req, res) => service.accept(req.body, req, res));
-app.use((err, res) => {
+terminal.post("/", (req, res) => service.accept(req.body, req, res));
+terminal.use((err, res) => {
   res.type("txt");
   res.status(500).send(err.stack);
 });
 
-app.listen(8448);
+terminal.listen(8448);
