b7e3d64091dab1f8eeb9281922a3c52f86e721bf
Can Mingir <canmingir@gmail.com>
2023-03-27 06:41:13 -0400

    Separate underlying Express.js

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 const runtime = require("./src/runtime");
-const express = require("express");
-const cors = require("cors");
 const parser = require("./src/libs/parser");
-const openapi = require("./src/libs/openapi");
-const fs = require("fs");
 const context = require("./src/libs/context");
 const terminal = require("./src/terminal");
 
@@ -40,56 +36,6 @@ function run(statement, p2, p3) {
   }
 }
 
-function accept(req, res, fn) {
-  const scope = { params: req.params, query: req.query, body: req.body };
-  const { result, error } = run(fn, scope, { details: true });
-  if (!result) res.status(404).end();
-  else if (error) res.status(400).json(result);
-  else res.status(200).json(result);
-}
-
-function app(options) {
-  const app = express();
-  app.use(express.json());
-  app.use(cors());
-  app.static = express.static;
-
-  let listener;
-
-  return {
-    express: () => app,
-    address: () => (listener ? listener.address() : null),
-    use: (...args) => app.use(...args),
-    get: (string, fn) => app.get(string, (req, res) => accept(req, res, fn)),
-    post: (string, fn) => app.post(string, (req, res) => accept(req, res, fn)),
-    put: (string, fn) => app.put(string, (req, res) => accept(req, res, fn)),
-    delete: (string, fn) =>
-      app.delete(string, (req, res) => accept(req, res, fn)),
-    listen: (port = 0, fn) => {
-      app.all("*", (req, res) => res.status(404).end());
-      // eslint-disable-next-line no-unused-vars
-      app.use((err, req, res, next) => res.status(500).send(err.stack));
-
-      start(options);
-      return (listener = app.listen(port, fn));
-    },
-    context: (path) => {
-      const file = fs.readFileSync(path, "utf8");
-      context.load(JSON.parse(file));
-    },
-    openapi: (path) => {
-      try {
-        const file = fs.readFileSync(path, "utf8");
-        openapi.initialize(app);
-        openapi.load(JSON.parse(file));
-      } catch (err) {
-        throw Error("Problem occurred while opening OpenAPI");
-      }
-    },
-  };
-}
-
-module.exports = app;
 module.exports.start = start;
 module.exports.register = register;
 module.exports.run = run;
---
+++ src/express.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const cors = require("cors");
+const fs = require("fs");
+const context = require("./libs/context");
+const openapi = require("./libs/openapi");
+const nucleoid = require("../index");
+
+function app(options) {
+  const app = express();
+  app.use(express.json());
+  app.use(cors());
+  app.static = express.static;
+
+  let listener;
+
+  return {
+    express: () => app,
+    address: () => (listener ? listener.address() : null),
+    use: (...args) => app.use(...args),
+    get: (string, fn) => app.get(string, (req, res) => accept(req, res, fn)),
+    post: (string, fn) => app.post(string, (req, res) => accept(req, res, fn)),
+    put: (string, fn) => app.put(string, (req, res) => accept(req, res, fn)),
+    delete: (string, fn) =>
+      app.delete(string, (req, res) => accept(req, res, fn)),
+    listen: (port = 0, fn) => {
+      app.all("*", (req, res) => res.status(404).end());
+      // eslint-disable-next-line no-unused-vars
+      app.use((err, req, res, next) => res.status(500).send(err.stack));
+
+      nucleoid.start(options);
+      return (listener = app.listen(port, fn));
+    },
+    context: (path) => {
+      const file = fs.readFileSync(path, "utf8");
+      context.load(JSON.parse(file));
+    },
+    openapi: (path) => {
+      try {
+        const file = fs.readFileSync(path, "utf8");
+        openapi.initialize(app);
+        openapi.load(JSON.parse(file));
+      } catch (err) {
+        throw Error("Problem occurred while opening OpenAPI");
+      }
+    },
+  };
+}
+
+function accept(req, res, fn) {
+  const scope = { params: req.params, query: req.query, body: req.body };
+  const { result, error } = nucleoid.run(fn, scope, { details: true });
+  if (!result) res.status(404).end();
+  else if (error) res.status(400).json(result);
+  else res.status(200).json(result);
+}
+
+module.exports = app;
