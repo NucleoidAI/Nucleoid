297fe15adcd6ee3242ec59525bb615a0d6acd6c3
Can Mingir <canmingir@gmail.com>
2023-06-08 14:21:21 -0400

    Export test libs

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const nucleoid = require("./src/nucleoid");
 const express = require("./src/express");
 const config = require("./src/config");
+const test = require("./src/lib/test");
 
 module.exports = express;
 module.exports.start = nucleoid.start;
 module.exports.register = nucleoid.register;
 module.exports.run = nucleoid.run;
 module.exports.config = config;
+module.exports.test = test;
