1840d6b8add3944389343b99357a7f67bdf55bc2
Can Mingir <canmingir@gmail.com>
2023-06-10 15:20:31 -0400

    Export OpenAPI lib

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const nucleoid = require("./src/nucleoid");
 const express = require("./src/express");
 const config = require("./src/config");
 const test = require("./src/lib/test");
+const openapi = require("./src/lib/openapi");
 
 module.exports = express;
 module.exports.start = nucleoid.start;
 module.exports.register = nucleoid.register;
 module.exports.run = nucleoid.run;
 module.exports.config = config;
+module.exports.openapi = openapi;
 module.exports.test = test;
