ac36bc2c68d663d1a0f3754d4988b1f1b8c86fc3
Can Mingir <canmingir@gmail.com>
2023-08-01 05:48:40 -0400

    Replace if-else statements with switch in stack

--- src/stack.js
+++ src/stack.js
@@ -28,223 +28,236 @@ function process(statements, prior, options = {}) {
     let instruction = instructions.shift();
     let statement = instruction.statement;
 
-    if (statement instanceof RETURN) {
-      let scope = instruction.scope;
-      return process(statement.statements, scope, options);
-    } else if (statement instanceof BREAK) {
-      let inst = instructions[0];
-
-      while (
-        inst !== undefined &&
-        statement.block !== undefined &&
-        inst.scope.block === statement.block
-      ) {
-        instructions.shift();
-        inst = instructions[0];
-        statement.block.break = true;
+    switch (true) {
+      case statement instanceof RETURN: {
+        let scope = instruction.scope;
+        return process(statement.statements, scope, options);
       }
-    } else if (statement instanceof EXPRESSION) {
-      const serialize = require("./lib/serialize");
-      let scope = instruction.scope;
+      case statement instanceof BREAK: {
+        let inst = instructions[0];
 
-      const tokens = statement.run(scope, false, false);
-
-      let value = String();
-      let transaction = false;
-
-      for (let i = 0; i < tokens.length; i++) {
-        const token = tokens[i];
-
-        if (token instanceof Token.EXPRESSION) {
-          const tmp = state.run(scope, token.construct());
-          value += serialize(tmp, "state");
-        } else if (token instanceof Token.CALL) {
-          transaction = true;
-          value += token.construct();
-        } else {
-          value += token.construct();
+        while (
+          inst !== undefined &&
+          statement.block !== undefined &&
+          inst.scope.block === statement.block
+        ) {
+          instructions.shift();
+          inst = instructions[0];
+          statement.block.break = true;
         }
+
+        break;
       }
+      case statement instanceof EXPRESSION: {
+        const serialize = require("./lib/serialize");
+        let scope = instruction.scope;
 
-      const run = state.run(scope, value, transaction);
+        const tokens = statement.run(scope, false, false);
 
-      if (instruction.scope === root && !instruction.derivative) {
-        result = run;
-      }
+        let value = String();
+        let transaction = false;
 
-      let list = statement.next(scope);
+        for (let i = 0; i < tokens.length; i++) {
+          const token = tokens[i];
 
-      if (list) {
-        instructions = list
-          .map((statement) => {
-            if (statement instanceof Instruction) {
-              return statement;
-            } else {
-              let scope = instruction.scope;
-              return new Instruction(
-                scope,
-                statement,
-                false,
-                true,
-                false,
-                null,
-                declarative
-              );
-            }
-          })
-          .concat(instructions);
-      }
-    } else if (statement instanceof $) {
-      if (instruction.before) {
-        statement.before(instruction.scope);
-      }
+          if (token instanceof Token.EXPRESSION) {
+            const tmp = state.run(scope, token.construct());
+            value += serialize(tmp, "state");
+          } else if (token instanceof Token.CALL) {
+            transaction = true;
+            value += token.construct();
+          } else {
+            value += token.construct();
+          }
+        }
 
-      if (instruction.run) {
-        let next = statement.run(instruction.scope);
-        next = Array.isArray(next) ? next : [next];
-
-        const scope = instruction.scope;
-        const { derivative } = instruction;
-
-        next = next
-          .map((statement) => {
-            return statement instanceof Instruction
-              ? statement
-              : new Instruction(scope, statement, true, true, true, null);
-          })
-          .map((instruction) => {
-            if (instruction.derivative === undefined) {
-              instruction.derivative = derivative;
-            }
-            return instruction;
-          });
-
-        instructions = next.filter((i) => !i.root).concat(instructions);
-        priorities = next.filter((i) => i.root).concat(priorities);
-      }
-    } else {
-      if (instruction.before) {
-        statement.before(instruction.scope);
-      }
-
-      if (instruction.run) {
-        let { value, next } = statement.run(instruction.scope) || {};
+        const run = state.run(scope, value, transaction);
 
         if (instruction.scope === root && !instruction.derivative) {
-          result = value;
+          result = run;
         }
 
-        if (next) {
-          let scope = instruction.scope;
+        let list = statement.next(scope);
+
+        if (list) {
+          instructions = list
+            .map((statement) => {
+              if (statement instanceof Instruction) {
+                return statement;
+              } else {
+                let scope = instruction.scope;
+                return new Instruction(
+                  scope,
+                  statement,
+                  false,
+                  true,
+                  false,
+                  null,
+                  declarative
+                );
+              }
+            })
+            .concat(instructions);
+        }
+
+        break;
+      }
+      case statement instanceof $: {
+        if (instruction.before) {
+          statement.before(instruction.scope);
+        }
+
+        if (instruction.run) {
+          let next = statement.run(instruction.scope);
           next = Array.isArray(next) ? next : [next];
 
-          if (statement instanceof BLOCK) {
-            scope = new Scope(scope);
-            scope.block = statement;
-          }
-
-          if (statement instanceof IF) {
-            scope = new Scope(scope);
-          }
+          const scope = instruction.scope;
+          const { derivative } = instruction;
 
           next = next
             .map((statement) => {
               return statement instanceof Instruction
                 ? statement
-                : new Instruction(scope, statement, true, true, true);
+                : new Instruction(scope, statement, true, true, true, null);
             })
-            .map((statement) => {
-              if (statement.derivative === undefined) {
-                statement.derivative = true;
+            .map((instruction) => {
+              if (instruction.derivative === undefined) {
+                instruction.derivative = derivative;
               }
-              return statement;
+              return instruction;
             });
 
           instructions = next.filter((i) => !i.root).concat(instructions);
           priorities = next.filter((i) => i.root).concat(priorities);
         }
-      }
 
-      skip: if (!(statement instanceof $)) {
-        if (statement.type === "CLASS" && instruction.scope.prior) {
-          break skip;
+        break;
+      }
+      default: {
+        if (instruction.before) {
+          statement.before(instruction.scope);
         }
 
-        if (instruction.graph) {
-          const { destroyed } = statement.beforeGraph(instruction.scope) || {};
+        if (instruction.run) {
+          let { value, next } = statement.run(instruction.scope) || {};
 
-          if (destroyed) {
-            continue;
+          if (instruction.scope === root && !instruction.derivative) {
+            result = value;
           }
 
-          if (statement instanceof Node) {
-            if (graph[statement.key || statement.id]) {
-              Node.replace(statement.key || statement.id, statement);
-            } else {
-              Node.register(statement);
+          if (next) {
+            let scope = instruction.scope;
+            next = Array.isArray(next) ? next : [next];
+
+            if (statement instanceof BLOCK) {
+              scope = new Scope(scope);
+              scope.block = statement;
             }
-          }
 
-          let list = statement.graph(instruction.scope);
+            if (statement instanceof IF) {
+              scope = new Scope(scope);
+            }
 
-          if (declarative) {
-            if (list) {
-              list.forEach((e) => {
-                if (graph[e].previous[statement.key] !== undefined) {
-                  throw ReferenceError("Circular Dependency");
+            next = next
+              .map((statement) => {
+                return statement instanceof Instruction
+                  ? statement
+                  : new Instruction(scope, statement, true, true, true);
+              })
+              .map((statement) => {
+                if (statement.derivative === undefined) {
+                  statement.derivative = true;
                 }
+                return statement;
               });
 
-              dependencies = dependencies.concat(
-                list.filter((e) => !dependencies.includes(e))
-              );
-            }
+            instructions = next.filter((i) => !i.root).concat(instructions);
+            priorities = next.filter((i) => i.root).concat(priorities);
           }
         }
 
-        if (statement.next) {
-          Object.values(statement.next)
-            .sort((a, b) => a.sequence - b.sequence)
-            .forEach((n) => {
-              let s = instruction.scope;
+        skip: if (!(statement instanceof $)) {
+          if (statement.type === "CLASS" && instruction.scope.prior) {
+            break skip;
+          }
 
-              if (n instanceof BLOCK || n instanceof IF) {
-                let scope = new Scope();
-                dependents.push(
-                  new Instruction(scope, n, false, true, false, null, true)
-                );
-                dependents.push(
-                  new Instruction(scope, n, false, false, true, null, true)
-                );
+          if (instruction.graph) {
+            const { destroyed } =
+              statement.beforeGraph(instruction.scope) || {};
+
+            if (destroyed) {
+              continue;
+            }
+
+            if (statement instanceof Node) {
+              if (graph[statement.key || statement.id]) {
+                Node.replace(statement.key || statement.id, statement);
               } else {
-                dependents.push(
-                  new Instruction(s, n, false, true, false, null, true)
+                Node.register(statement);
+              }
+            }
+
+            let list = statement.graph(instruction.scope);
+
+            if (declarative) {
+              if (list) {
+                list.forEach((e) => {
+                  if (graph[e].previous[statement.key] !== undefined) {
+                    throw ReferenceError("Circular Dependency");
+                  }
+                });
+
+                dependencies = dependencies.concat(
+                  list.filter((e) => !dependencies.includes(e))
                 );
               }
-            });
-        }
-
-        // Root scope is a scope, which does not have any prior
-        if (!instruction.scope.prior) {
-          if (!instruction.statement.skip) {
-            dependencies.forEach((source) => {
-              let targetKey = statement.key ? statement.key : statement.id;
-              Node.direct(source, targetKey, statement);
-            });
+            }
           }
 
-          instructions = instructions.concat(dependents);
-          instructions = instructions.concat(priorities);
+          if (statement.next) {
+            Object.values(statement.next)
+              .sort((a, b) => a.sequence - b.sequence)
+              .forEach((n) => {
+                let s = instruction.scope;
 
-          dependencies = [];
-          dependents = [];
-          priorities = [];
+                if (n instanceof BLOCK || n instanceof IF) {
+                  let scope = new Scope();
+                  dependents.push(
+                    new Instruction(scope, n, false, true, false, null, true)
+                  );
+                  dependents.push(
+                    new Instruction(scope, n, false, false, true, null, true)
+                  );
+                } else {
+                  dependents.push(
+                    new Instruction(s, n, false, true, false, null, true)
+                  );
+                }
+              });
+          }
+
+          // Root scope is a scope, which does not have any prior
+          if (!instruction.scope.prior) {
+            if (!instruction.statement.skip) {
+              dependencies.forEach((source) => {
+                let targetKey = statement.key ? statement.key : statement.id;
+                Node.direct(source, targetKey, statement);
+              });
+            }
+
+            instructions = instructions.concat(dependents);
+            instructions = instructions.concat(priorities);
+
+            dependencies = [];
+            dependents = [];
+            priorities = [];
+          }
         }
-      }
 
-      if (instruction.scope.block) {
-        instruction.scope.block.stage(instruction);
-        statement.block = instruction.scope.block;
+        if (instruction.scope.block) {
+          instruction.scope.block.stage(instruction);
+          statement.block = instruction.scope.block;
+        }
       }
     }
   }
