c83258f82058510144eff4acbfdebc51c3fd014f
Invademars <86682731+Invademars@users.noreply.github.com>
2025-01-08 07:19:59 +0530

    Addedpython -u c:\Users\Gitansh\Downloads\Nucleoid-main\python\tests\test_nucleoid1.py

---
+++ python/tests/test_nucleoid.py
@@ -0,0 +1,13 @@
+import pytest
+from nucleoid import Nucleoid  # Import the Nucleoid class from the nucleoid module
+
+@pytest.fixture
+def setup():
+    # Initialize the Nucleoid class
+    return Nucleoid()
+
+def test_give_variable_value(setup):
+    # Use the initialized Nucleoid instance
+    nucleoid = setup
+    nucleoid.run("i = 1")
+    assert nucleoid.run("i == 1") is True
